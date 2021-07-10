ae947a1002fdf29f623df37831056ea8042d7697
alecamar <acmartensen@gmail.com>
2021-07-10 14:49:02 -0300

    subindo pacote

---
+++ R/createRepo.R
@@ -0,0 +1,27 @@
+#' Função para criar um repositório padrão
+#'
+#' Essa função cria um diretório padrão, com 8 pastas e um README.md 
+#' @param n defaults=empty
+#' @keywords repository
+#' @export
+#' @examples
+#' createRepo()
+
+
+
+
+createRepo<-function(n){
+  #dir.create(nome)
+  #setwd(paste("./", nome, sep=""))
+  dir.create("data")
+  dir.create("data_use")
+  dir.create("scripts")
+  dir.create("figs")
+  dir.create("outputs")
+  dir.create("docs")
+  dir.create("Rfunctions")
+  dir.create("vignettes")
+  fileConn<-file("README.txt")
+  writeLines(paste("README", nome, sep="_"), fileConn)
+  close(fileConn)
+}
